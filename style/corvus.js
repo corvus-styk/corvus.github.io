73048786ec0555fd37eb4316fb71a97492f5a86a
corvus-styk <corvus.styk.2025@gmail.com>
2025-08-08 17:41:30 +0900

    top-page css fix

--- style/corvus.js
+++ style/corvus.js
@@ -9,7 +9,6 @@ function goTo(url) {
 
   window.addEventListener('DOMContentLoaded', () => {
     const wrapper = document.getElementsByClassName('about-images')[0];
-	// console.log(wrapper)
     const images = wrapper.querySelectorAll('img');
 
     let maxBottom = 0;
